31cf6517bf62b31884ce72fc266ea986493e0b69
repinek <137826826+repinek@users.noreply.github.com>
2026-06-05 15:29:23 +0500

    refactor: add base-section logic (createTitle), fix ref get value, refactor section a little

--- src/mod/ui/section.ts
+++ src/mod/ui/section.ts
@@ -1,17 +1,22 @@
-import { Texture2D } from "../../engine/wrappers/texture";
 import { SonolusAssets } from "../../sonolus/wrappers/assets";
 import { Dep } from "../../sonolus/wrappers/reactivity/dep";
 import { SonolusRouteSection } from "../../sonolus/wrappers/routing/route-section";
 import { SonolusRows } from "../../sonolus/wrappers/ui/common/rows";
+import { SonolusBaseSection } from "../../sonolus/wrappers/ui/common/sections/base-section";
 import { SonolusCustomSection } from "../../sonolus/wrappers/ui/common/sections/custom-section";
+import { I18n } from "../i18n/i18n";
 
 export class CustomSection {
+    private static readonly SECTION_ICON_NAME: string = "IconStar";
+
     static buildCustomSection(): SonolusRouteSection {
-        const rows = SonolusRows.new().Gap(20).Children([]);
+        const Title = SonolusBaseSection.CreateTitle(I18n.tRef("ui.title"));
+
+        const rows = SonolusRows.new().Gap(20).Children([Title]);
 
         const section = SonolusCustomSection.new().Content(rows);
 
-        const icon: Dep<Texture2D> = Dep.opImplicit(SonolusAssets.getAsset("IconStar"));
+        const icon = Dep.opImplicit(SonolusAssets.getAsset(this.SECTION_ICON_NAME));
         const route = SonolusRouteSection.new(icon, section);
         return route;
     }
--- src/sonolus/wrappers/ui/common/sections/base-section.ts
+++ src/sonolus/wrappers/ui/common/sections/base-section.ts
@@ -1,3 +1,28 @@
+import { AssemblyHelper } from "../../../../../engine/assembly-helper";
+import { Dep } from "../../../reactivity/dep";
 import { SonolusCompositeWidget } from "../../composite-widget";
+import { SonolusWidget } from "../../widget";
 
-export class SonolusBaseSection extends SonolusCompositeWidget {}
+interface Api {
+    SectionBase: Il2Cpp.Class;
+}
+
+export class SonolusBaseSection extends SonolusCompositeWidget {
+    private static _apiBase: Api | null = null;
+
+    private static apiBase(): Api {
+        if (this._apiBase) return this._apiBase;
+
+        const SectionBase = AssemblyHelper.AssemblyCSharp.class("Sonolus.UI.Common.Sections.SectionBase");
+
+        this._apiBase = {
+            SectionBase
+        };
+
+        return this._apiBase;
+    }
+
+    static CreateTitle(text: Dep<Il2Cpp.String>): SonolusWidget {
+        return this.apiBase().SectionBase.method<SonolusWidget>("CreateTitle", 1).invoke(text);
+    }
+}
