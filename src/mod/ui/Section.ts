2d9cf7202a290bfb622eb363db326db3531b535e
repinek <137826826+repinek@users.noreply.github.com>
2026-06-06 00:00:44 +0500

    refactor: Tests in Section.ts

--- src/mod/ui/Section.ts
+++ src/mod/ui/Section.ts
@@ -1,7 +1,10 @@
 import { SonolusAssets } from "../../sonolus/wrappers/Assets";
 import { Dep } from "../../sonolus/wrappers/reactivity/Dep";
+import { Ref } from "../../sonolus/wrappers/reactivity/Ref";
 import { SonolusRouteSection } from "../../sonolus/wrappers/routing/RouteSection";
 import { SonolusBtnField } from "../../sonolus/wrappers/ui/common/fields/BtnField";
+import { SonolusToggleField } from "../../sonolus/wrappers/ui/common/fields/ToggleField";
+import { SonolusTxtInputField } from "../../sonolus/wrappers/ui/common/fields/TxtInputField";
 import { SonolusImgLblBtn } from "../../sonolus/wrappers/ui/common/ImgLblBtn";
 import { SonolusRows } from "../../sonolus/wrappers/ui/common/Rows";
 import { SonolusBaseSection } from "../../sonolus/wrappers/ui/common/sections/BaseSection";
@@ -12,23 +15,45 @@ import { I18n } from "../i18n/I18n";
 export class CustomSection {
     private static readonly SECTION_ICON_NAME: string = "IconStar";
 
-    private static buildTestField(): SonolusBtnField {
+    private static buildTestBtnField(): SonolusBtnField {
         const btn = SonolusImgLblBtn.new()
             .Title(I18n.tRef("button"))
             .Icon(Dep.opImplicit(SonolusAssets.getAsset("IconStar")))
             .OnClick(() => {
                 Logger.debug("Clicked");
-            });
+            })
+            .validate();
 
-        return SonolusBtnField.new().Title(I18n.tRef("hi")).Description(I18n.tRef("desc")).Value(I18n.tRef("")).Btns([btn]);
+        return SonolusBtnField.new().Title(I18n.tRef("hi")).Value(I18n.tRef("button2")).Btns([btn]).validate();
+    }
+
+    private static buildTestToggleField(): SonolusToggleField {
+        return SonolusToggleField.new().Title(I18n.tRef("hi")).Description(I18n.tRef("desc")).Value(Ref.create(false)).DefaultValue(false).validate();
+    }
+
+    private static buildTestTxtInputField(): SonolusTxtInputField {
+        return SonolusTxtInputField.new()
+            .Title(I18n.tRef("hi"))
+            .Description(I18n.tRef("desc"))
+            .Value(Ref.create("value"))
+            .DefaultValue(Ref.create("initialValue"))
+            .Icon(Dep.opImplicit(SonolusAssets.getAsset("IconStar")))
+            .Placeholder(Ref.create("placeholder"))
+            .Suffix(Ref.create("uwu"))
+            .Enabled(Ref.create(true))
+            .Shortcuts(["hi", "wow!", "uwu"])
+            .CharacterLimit(12)
+            .validate();
     }
 
     static buildCustomSection(): SonolusRouteSection {
         const Title = SonolusBaseSection.CreateTitle(I18n.tRef("ui.title"));
 
-        const rows = SonolusRows.new().Gap(20).Children([Title, CustomSection.buildTestField()]);
+        const rows = SonolusRows.new()
+            .Gap(20)
+            .Children([Title, CustomSection.buildTestBtnField(), CustomSection.buildTestToggleField(), CustomSection.buildTestTxtInputField()]);
 
-        const section = SonolusCustomSection.new().Content(rows);
+        const section = SonolusCustomSection.new().Content(rows).validate();
 
         const icon = Dep.opImplicit(SonolusAssets.getAsset(this.SECTION_ICON_NAME));
         const route = SonolusRouteSection.new(icon, section);
