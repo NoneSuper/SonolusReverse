9b8970f7c67ccb61b58a7d017f3f45eb5bf163ae
repinek <137826826+repinek@users.noreply.github.com>
2026-06-07 14:53:45 +0500

    refactor: add comments

--- src/sonolus/ui/Title.ts
+++ src/sonolus/ui/Title.ts
@@ -2,37 +2,17 @@ import { AssemblyHelper } from "../../engine/AssemblyHelper";
 import { Logger } from "../../utils/Logger";
 
 export class SonolusUITitle {
-    /*
-    private static _api: Api | null = null;
-    
-    private static api(): Api {
-        if (this._api) return this._api;
-
-        const Asm = AssemblyHelper.AssemblyCSharp;
-
-        // const Lbl = AssemblyHelper.AssemblyCSharp.class("Sonolus.UI.Common.Lbl");
-        const Title = AssemblyHelper.AssemblyCSharp.class("Sonolus.UI.Title");
-
-        const Titl
-        this._api = {
-            // Lbl,
-            TitleSetup
-        }
-
-        return this._api;
-    }
-    */
-
     private static _inTitleSetup: boolean = false;
 
     static init(): void {
         const module = this;
 
         const Title = AssemblyHelper.AssemblyCSharp.class("Sonolus.UI.Title");
+
         Title.method<Il2Cpp.Object>("Setup", 0).implementation = function (): Il2Cpp.Object {
-            Logger.hook("Title::Setup called");
+            Logger.hook("Title::Setup called"); // <---- OnEnter
             module._inTitleSetup = true;
-            const widget = this.method<Il2Cpp.Object>("Setup", 0).invoke();
+            const widget = this.method<Il2Cpp.Object>("Setup", 0).invoke(); // ---> OnLeave
             module._inTitleSetup = false;
             return widget;
         };
