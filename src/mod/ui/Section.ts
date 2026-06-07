22f89cfd57ca84ebf85d4e31982f442734f4e1ef
repinek <137826826+repinek@users.noreply.github.com>
2026-06-07 13:06:03 +0500

    refactor: remove old section tests

--- src/mod/ui/Section.ts
+++ src/mod/ui/Section.ts
@@ -12,39 +12,6 @@ import { I18n } from "../i18n/I18n";
 export class CustomSection {
     private static readonly SECTION_ICON_NAME: string = "IconStar";
 
-    /*
-    private static buildTestBtnField(): SonolusBtnField {
-        const btn = SonolusImgLblBtn.new()
-            .Title(I18n.tRef("button"))
-            .Icon(Dep.opImplicit(SonolusAssets.getAsset("IconStar")))
-            .OnClick(() => {
-                Logger.debug("Clicked");
-            })
-            .validate();
-
-        return SonolusBtnField.new().Title(I18n.tRef("hi")).Value(I18n.tRef("button2")).Btns([btn]).validate();
-    }
-
-    private static buildTestToggleField(): SonolusToggleField {
-        return SonolusToggleField.new().Title(I18n.tRef("hi")).Description(I18n.tRef("desc")).Value(Ref.create(false)).DefaultValue(false).validate();
-    }
-
-    private static buildTestTxtInputField(): SonolusTxtInputField {
-        return SonolusTxtInputField.new()
-            .Title(I18n.tRef("hi"))
-            .Description(I18n.tRef("desc"))
-            .Value(Ref.create("value"))
-            .DefaultValue(Ref.create("initialValue"))
-            .Icon(Dep.opImplicit(SonolusAssets.getAsset("IconStar")))
-            .Placeholder(Ref.create("placeholder"))
-            .Suffix(Ref.create("uwu"))
-            .Enabled(Ref.create(true))
-            .Shortcuts(["hi", "wow!", "uwu"])
-            .CharacterLimit(12)
-            .validate();
-    }
-    */
-
     static buildCustomSection(): SonolusRouteSection {
         const title = this.title();
         const spoofField = this.spoofField();
