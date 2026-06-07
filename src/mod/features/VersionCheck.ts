48fd5d99dc681aeb731bf476f0a86a0db8823b4c
repinek <137826826+repinek@users.noreply.github.com>
2026-06-07 14:54:52 +0500

    refactor: use UPPER_SNAKE_CASE for readonly

--- src/mod/features/VersionCheck.ts
+++ src/mod/features/VersionCheck.ts
@@ -3,14 +3,14 @@ import { Config } from "../data/Config";
 
 // https://wiki.sonolus.com/custom-server-specs/headers/sonolus-version
 export class VersionCheck {
-    private static readonly versionHeaderName = "Sonolus-Version";
+    private static readonly VERSION_HEADER_NAME = "Sonolus-Version";
 
     static init(): void {
         const UnityWebRequest = AssemblyHelper.UnityWebRequestModule.class("UnityEngine.Networking.UnityWebRequest");
 
         // @ts-ignore
         UnityWebRequest.method<Il2Cpp.String>("GetResponseHeader", 1).implementation = function (name: Il2Cpp.String): Il2Cpp.String | NativePointer {
-            if (Config.versionCheck && !name.isNull() && name.content === VersionCheck.versionHeaderName) {
+            if (Config.versionCheck && !name.isNull() && name.content === VersionCheck.VERSION_HEADER_NAME) {
                 return ptr(0); // or Il2Cpp.string(""); but it's useless allocate new System.String object for that
             }
             return this.method<Il2Cpp.String>("GetResponseHeader", 1).invoke(name);
