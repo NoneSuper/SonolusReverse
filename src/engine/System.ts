49ebcefb493dfb6bbe61aa558ac7997b51382b73
repinek <137826826+repinek@users.noreply.github.com>
2026-06-14 00:25:59 +0500

    style(System): use ??=, add System.Func`1

--- src/engine/System.ts
+++ src/engine/System.ts
@@ -4,17 +4,18 @@ export class System {
     private static _string: Il2Cpp.Class;
     private static _boolean: Il2Cpp.Class;
     private static _action: Il2Cpp.Class;
+    private static _func1: Il2Cpp.Class;
 
     static get String(): Il2Cpp.Class {
-        if (!this._string) this._string = Il2Cpp.corlib.class("System.String");
-        return this._string;
+        return (this._string ??= Il2Cpp.corlib.class("System.String"));
     }
     static get Boolean(): Il2Cpp.Class {
-        if (!this._boolean) this._boolean = Il2Cpp.corlib.class("System.Boolean");
-        return this._boolean;
+        return (this._boolean ??= Il2Cpp.corlib.class("System.Boolean"));
     }
     static get Action(): Il2Cpp.Class {
-        if (!this._action) this._action = Il2Cpp.corlib.class("System.Action");
-        return this._action;
+        return (this._action ??= Il2Cpp.corlib.class("System.Action"));
+    }
+    static get Func1(): Il2Cpp.Class {
+        return (this._func1 ??= Il2Cpp.corlib.class("System.Func`1"));
     }
 }
